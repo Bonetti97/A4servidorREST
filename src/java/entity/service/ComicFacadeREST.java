f29201c98dda5b65df428b5781cd28a2278f33de
Bonetti97 <javiboned@gmail.com>
2018-12-03 19:56:51 +0100

    Servidor

--- src/java/entity/service/ComicFacadeREST.java
+++ src/java/entity/service/ComicFacadeREST.java
@@ -6,10 +6,19 @@
 package entity.service;
 
 import entity.Comic;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -22,7 +31,7 @@ import javax.ws.rs.core.MediaType;
 
 /**
  *
- * @author ubuntu
+ * @author sergiorosadoferrer
  */
 @Stateless
 @Path("entity.comic")
@@ -36,15 +45,19 @@ public class ComicFacadeREST extends AbstractFacade<Comic> {
     }
 
     @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_JSON})
-    public void create(Comic entity) {
-        super.create(entity);
+    @Path("{nombre}/{descripcion}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(@PathParam("nombre") String nombre, @PathParam("descripcion") String descripcion) {
+        Comic c = new Comic();
+        c.setDescripcion(descripcion);
+        c.setNombre(nombre);
+        c.setFechaCreacion(new Date());
+        super.create(c);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Comic entity) {
         super.edit(entity);
     }
@@ -61,17 +74,100 @@ public class ComicFacadeREST extends AbstractFacade<Comic> {
     public Comic find(@PathParam("id") Integer id) {
         return super.find(id);
     }
+    
+     @GET
+    @Path("buscaFecha/{fecha}")
+    @Produces({MediaType.APPLICATION_JSON})
+     public List<Comic> buscarFecha (@PathParam("fecha") String d){
+         String f=d.substring(0, 10);
+       
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+            Query q=this.em.createQuery("Select c from Comic c where c.fechaCreacion >= :fecha");
+        try {       
+         Date date =format.parse(f);
+        q.setParameter("fecha", format);
+        } catch (ParseException ex) {
+            Logger.getLogger(ComicFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return q.getResultList();
+    }
+    
+    @GET
+    @Path("buscaNombre/{nombre}")
+    @Produces({ MediaType.APPLICATION_JSON})
+      public List<Comic> buscarNombre(@PathParam("nombre") String nombre){    
+        Query q= this.em.createQuery("SELECT c from Comic c where c.nombre LIKE :nombre");
+        q.setParameter("nombre","%"+ nombre+"%");
+        List<Comic> lista = (List<Comic>)q.getResultList();
+        if(lista.isEmpty()){
+            return new ArrayList<>();
+        }else{
+            return lista;
+        }
+    }
 
     @GET
+    @Path("findAll")
     @Override
     @Produces({MediaType.APPLICATION_JSON})
     public List<Comic> findAll() {
         return super.findAll();
     }
+    
+    @GET
+    @Path("ordenaComicFecha")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Comic> ordenarFecha(){
+        Query q= this.em.createQuery("SELECT c FROM Comic c ORDER BY c.fechaCreacion DESC");
+         List<Comic> lista = (List<Comic>)q.getResultList();
+        if(lista.isEmpty()){
+            return new ArrayList<>();
+        }else{
+            return lista;
+        } 
+    }
+    
+    
+    @GET
+    @Path("ordenaComicAlfabetico")
+    @Produces({ MediaType.APPLICATION_JSON})
+      public List<Comic> ordenarAlfabetico(){
+        Query q = this.em.createQuery("SELECT c FROM Comic c ORDER BY c.nombre ");
+         List<Comic> lista = (List<Comic>)q.getResultList();
+        if(lista.isEmpty()){
+            return new ArrayList<>();
+        }else{
+            return lista;
+        }
+    }
+      
+    @GET
+    @Path("ordenaComicEntrega")
+    @Produces({MediaType.APPLICATION_JSON})
+      public List<Comic> ordenarPorEntregas(){
+        Query q= this.em.createNativeQuery("SELECT c.* FROM Comic as c JOIN Entrega AS e on c.idComic=e.idComic GROUP BY e.idComic ORDER BY count(e.idEntrega) DESC");
+        List<Object[]> lista = (List<Object[]>)q.getResultList();
+        Iterator<Object[]> it = lista.iterator();
+        List<Comic> listaC = new ArrayList<>();
+        if(!lista.isEmpty()){
+            while(it.hasNext()){
+            Comic co = this.find(it.next()[0]);
+            listaC.add(co);
+            }
+        }    
+        if(listaC.isEmpty()){
+            return new ArrayList<>();
+        }else{
+            return listaC;
+        }    
+    }
+    
+      
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Comic> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
--- src/java/entity/service/EntregaFacadeREST.java
+++ src/java/entity/service/EntregaFacadeREST.java
@@ -5,11 +5,18 @@
  */
 package entity.service;
 
+import com.sun.xml.wss.impl.misc.Base64;
+import entity.Comic;
 import entity.Entrega;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.jws.WebParam;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -22,14 +29,19 @@ import javax.ws.rs.core.MediaType;
 
 /**
  *
- * @author ubuntu
+ * @author sergiorosadoferrer
  */
 @Stateless
 @Path("entity.entrega")
 public class EntregaFacadeREST extends AbstractFacade<Entrega> {
 
+    @EJB
+    private ComicFacadeREST comicFacadeREST;
+
     @PersistenceContext(unitName = "A4servidorREST")
     private EntityManager em;
+    
+    
 
     public EntregaFacadeREST() {
         super(Entrega.class);
@@ -37,7 +49,7 @@ public class EntregaFacadeREST extends AbstractFacade<Entrega> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void create(Entrega entity) {
         super.create(entity);
     }
@@ -57,13 +69,76 @@ public class EntregaFacadeREST extends AbstractFacade<Entrega> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public Entrega find(@PathParam("id") Integer id) {
         return super.find(id);
     }
+     
+      @GET
+    @Path("ordenaEntregaFecha/{idComic}")
+    @Produces({ MediaType.APPLICATION_JSON})
+      public List<Entrega> ordenarFecha2(@PathParam("idComic") Integer idComic){
+        Comic c= this.comicFacadeREST.find(idComic);
+        Query q= this.em.createQuery("SELECT e FROM Entrega e WHERE e.idComic =:comic ORDER BY e.fechaCreacion ASC");
+        q.setParameter("comic", c);
+        List<Entrega> lista = (List<Entrega>)q.getResultList();
+        if(lista.isEmpty()){
+            return new ArrayList<>();
+        }else{
+            return lista;
+        } 
+    }  
+      
+      
+    @GET
+    @Path("ordenaEntregaNombreInverso/{idComic}")
+    @Produces({ MediaType.APPLICATION_JSON})
+      public List <Entrega> ordenarNombreInverso(@PathParam("idComic") Integer idComic){
+            Comic c= this.comicFacadeREST.find(idComic);
+          Query q= this.em.createQuery("SELECT e FROM Entrega e WHERE e.idComic =:comic ORDER BY e.nombre DESC ");
+         q.setParameter("comic", c);
+          List<Entrega> lista = (List<Entrega>)q.getResultList();
+          if(lista.isEmpty()){
+            return new ArrayList<>();
+          }else{
+            return lista;
+          }
+      } 
+      
+    @GET
+    @Path("filtraEntregaFecha/{idComic}/{fecha}")
+    @Produces({ MediaType.APPLICATION_JSON})
+      public List<Entrega> filtrarPorFecha(@PathParam("idComic") Integer idComic, @PathParam("fecha") Date fecha){
+          Comic c= this.comicFacadeREST.find(idComic);
+          Query q= this.em.createQuery("SELECT e FROM Entrega e where (e.fechaCreacion >= :fecha AND e.idComic =:comic)");
+          q.setParameter("comic", c);
+          q.setParameter("fecha", fecha);
+          List<Entrega> lista = (List<Entrega>)q.getResultList();
+          if(lista.isEmpty()){
+            return new ArrayList<>();
+          }else{
+            return lista;
+          } 
+      } 
+      
+    @GET
+    @Path("filtraEntregaComic/{idComic}")
+    @Produces({MediaType.APPLICATION_JSON})
+      public List<Entrega> entregasPorComic(@PathParam("idComic") Integer idComic){
+          Comic c= this.comicFacadeREST.find(idComic);
+          Query q = this.em.createQuery("SELECT e FROM Entrega e where e.idComic = :comic");
+          q.setParameter("comic", c);
+           List<Entrega> lista = (List<Entrega>)q.getResultList();
+          if(lista.isEmpty()){
+            return new ArrayList<>();
+          }else{
+            return lista;
+          } 
+      } 
+      
 
     @GET
-    @Override
+    @Path ("findAll")
     @Produces({MediaType.APPLICATION_JSON})
     public List<Entrega> findAll() {
         return super.findAll();
@@ -71,7 +146,7 @@ public class EntregaFacadeREST extends AbstractFacade<Entrega> {
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Entrega> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
@@ -82,6 +157,15 @@ public class EntregaFacadeREST extends AbstractFacade<Entrega> {
     public String countREST() {
         return String.valueOf(super.count());
     }
+    
+    @GET
+    @Path("getFoto/{idFoto}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String fotoBase64(@PathParam("idFoto") Integer idFoto){
+     Entrega nuevaEntrega=super.find(idFoto);
+         String fotoB64=Base64.encode(nuevaEntrega.getArchivo());
+         return fotoB64;
+      }
 
     @Override
     protected EntityManager getEntityManager() {
